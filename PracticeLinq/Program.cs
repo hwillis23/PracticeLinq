537e37d31c74419ca83484c8f04bf969dc1f119e
Hannah Willis <hwillis8817@gmail.com>
2022-04-06 15:55:06 -0500

    added bonus methods

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -11,13 +11,11 @@ namespace PracticeLinq
             List<string> games = new List<string>
             {"Donkey Kong Country", "Mario World","Legends of Zelda","Super Mario Cart", "NBA","Duck Hunt","Mortal Kombat"};
 
-            var games2 = games.OrderBy(name => name.Length).OrderBy(name => name.Length).ToList();  
+            var games2 = games.Where(name => name.Contains ("t")).OrderBy(name => name.Length).OrderBy(name => name.Length).ToList();  
             foreach (var game in games2)
             {
                 Console.WriteLine(game);
             }    
-
-
 
         }
     }
@@ -27,4 +25,5 @@ namespace PracticeLinq
 //Create a list of video game names...///// don't forget to use foreach because it's a list to call it with CWL
 //Order the list of games by length of the game name.- use OrderBy() 
 //Use the lambda expression => in this exercise as well.
+//Bonus - Try to use each of these methods on your list of video games
 //use Method Syntax for this exercise //
